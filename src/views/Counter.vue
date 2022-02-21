a27fedd51fdd08efcfb867ffa6392e0b3dec19c9
Ricardo Evz <ricardo.esp.vz@gmail.com>
2022-02-20 23:48:38 -0600

    primer commit

---
+++ src/views/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Counter vue</h1>
+    <h3>{{ counter }}</h3>
+
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+</template>
+
+<script>
+import useCounter from "@/composables/useCounter";
+
+export default {
+    name: "Counter",
+    props: {},
+    emits: [],
+    setup() {
+        const { counter, increase, decrease } = useCounter(25);
+
+        return {
+            counter,
+            increase,
+            decrease,
+        };
+    },
+};
+</script>
+
+<style>
+</style>
